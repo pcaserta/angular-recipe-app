b0581355d10dcc3c698a4aa1bd469729d0a40f9c
pcaserta <patrick.caserta@live.longwood.edu>
2020-01-31 16:57:12 -0500

    addToShoppingList working

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,38 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
-
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
-    
-    private recipes: Recipe[] = [
-        new Recipe("Spaghetti", "This is italian", 'https://www.starfrit.com/media/contentmanager/content/cache/1070x1070//recipes/e1_r1_spaghetti.jpg'),
-        new Recipe("Meatballs", "This is italian", 'https://www.starfrit.com/media/contentmanager/content/cache/1070x1070//recipes/e1_r1_spaghetti.jpg')
-      ];
 
-      getRecipes() {
-          return this.recipes.slice();
-      }
+    private recipes: Recipe[] = [
+        new Recipe(
+            "Spaghetti", 
+            "This is italian",
+             'https://www.starfrit.com/media/contentmanager/content/cache/1070x1070//recipes/e1_r1_spaghetti.jpg',
+             [
+                new Ingredient('Pasta', 2),
+                new Ingredient('tomatoe sauce', 3)
+             ]),
+        new Recipe(
+            "Meatballs",
+            "This is more italian", 
+            'https://www.flavcity.com/wp-content/uploads/2018/06/meatballs-tomato-sauce.jpg',
+            [
+                new Ingredient('parmesean', 6),
+                new Ingredient('beef', 2)
+            ]
+            )];
+
+    constructor(private slService: ShoppingListService){}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients)
+    }
 }

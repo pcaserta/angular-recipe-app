00732406bc380d11cd3b18b2c1e55e73ee7dbb38
pcaserta <patrick.caserta@live.longwood.edu>
2020-02-28 19:04:43 -0500

    auth interceptor added for saving and storing data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -23,27 +23,20 @@ export class DataStorageService {
       });
   }
   fetchRecipes() {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.http.get<Recipe[]>(
-          "https://recipe-db-6b3e5.firebaseio.com/recipes.json",
-          {
-            params: new HttpParams().set("auth", user.token)
-          }
-        );
-      }),
-      map(recipes => {
-        return recipes.map(recipe => {
-          return {
-            ...recipe,
-            ingredients: recipe.ingredients ? recipe.ingredients : []
-          };
-        });
-      }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes);
-      })
-    );
+    return this.http
+      .get<Recipe[]>("https://recipe-db-6b3e5.firebaseio.com/recipes.json")
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
